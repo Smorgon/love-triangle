b276de67417c7827fbdec7ffd27cd431903022aa
Mikhail Rohau <misha_rogov@tut.by>
2018-02-22 01:35:08 +0300

    'love-triangle'

--- src/index.js
+++ src/index.js
@@ -3,5 +3,33 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+    for (let i = 0; i < preferences.length; i++) {
+        let first = i + 1;
+        let second = preferences[first - 1];
+        if (second <= first) {
+            continue;
+        }
+
+        let third = preferences[second - 1];
+        if (third <= first) {
+            continue;
+        }
+
+        if (first === preferences[third - 1]) {
+            count++;
+        }
+
+        //This is other variant, but I think that the first variant of code more readable
+        /*if (preferences[i] <= i + 1) {
+            continue;
+        }
+        if (preferences[preferences[i] - 1] <= i + 1) {
+            continue;
+        }
+        if (i + 1 === preferences[preferences[preferences[i] - 1] - 1]) {
+            count++;
+        }*/
+    }
+    return count;
 };
